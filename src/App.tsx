99287b3eda2e6bda4e2ef9cfa1aded6db929c6a7
Hessam Imen <hessam.imen@gmail.com>
2023-02-25 19:50:01 +1100

    feat: The Footer component is implemented and styled

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import { SelectedPage } from "@/shared/types";
 import Benefits from "@/scenes/benefits";
 import OurClasses from "@/scenes/ourClasses";
 import ContactUs from "./scenes/contactUs";
+import Footer from "./scenes/footer";
 
 function App() {
   const [selectedPage, setSelectedPage] = useState<SelectedPage>(
@@ -35,6 +36,7 @@ function App() {
       <Benefits setSelectedPage={setSelectedPage} />
       <OurClasses setSelectedPage={setSelectedPage} />
       <ContactUs setSelectedPage={setSelectedPage} />
+      <Footer />
     </div>
   );
 }
